2bf9d0cbab4e13f10c7465b8f604c7c70a99f43a
eons-shubham <eons.shubham@gmail.com>
2023-11-06 11:47:01 +0530

    Return Value from Async Function ( Axiox )

--- src/StockData.js
+++ src/StockData.js
@@ -13,17 +13,22 @@ export default class StockData extends React.Component{
     fetchAPI = async () => {
         const URL = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo`;
         const response = await axios.get(URL);
-        
+        return await response.data;
     }
 
-    componentDidMount(){
-        let x = this.fetchAPI();
+    abc = async () => {
+        let x = await this.fetchAPI();
         console.log(x)
     }
 
+    componentDidMount(){
+        this.abc();
+    }
+
+
     render(){
         return (
-            <h3 onClick={}>hello</h3>
+            <h3 onClick={this.abc}> hello</h3>
         )
     }
 
