f9badbe744388b92d4a846d3ef801911cdd1e2b3
eons-shubham <eons.shubham@gmail.com>
2023-11-09 11:35:28 +0530

    Appointment Booking Complete

--- src/app.js
+++ src/app.js
@@ -1,35 +1,9 @@
-import "./style.css";
-import Dog from "./Dog.js";
-import React from "react";
+import BookingForm from "./BookingForm.js";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: "random"
-    };
-  }
-
-  update = (e) => {
-    this.setState((state, props) => {
-      return { id: e.target.value };
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <select onChange={this.update}>
-          <option value="random">Random</option>
-          <option value="beagle">Beagle</option>
-          <option value="boxer">Boxer</option>
-          <option value="dalmatian">Dalmatian</option>
-          <option value="husky">Husky</option>
-        </select>
-
-        <Dog id={this.state.id} />
-        
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <BookingForm />
+    </div>
+  );
 }
